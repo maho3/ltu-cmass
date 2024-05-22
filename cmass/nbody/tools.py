03f00e9121e452f7700acbed58087206c0df0a0c
Matthew Ho <matthew.annam.ho@gmail.com>
2024-05-22 10:35:38 +0200

    adding pylians field interpolation to supplant pmwd. untested

--- cmass/nbody/tools.py
+++ cmass/nbody/tools.py
@@ -2,8 +2,8 @@ import os
 from os.path import join as pjoin
 import logging
 import numpy as np
-from ..utils import timing_decorator
-from pmwd import Configuration, Particles, scatter
+from ..utils import timing_decorator, get_particle_mass
+import MAS_library as MASL
 
 
 @timing_decorator
@@ -41,8 +41,63 @@ def save_nbody(savedir, rho, fvel, pos, vel,
     logging.info(f'Saved to {savedir}.')
 
 
+def assign_field(pos, BoxSize, Ngrid, MAS, value=None, verbose=False):
+    """ Assign particle positions to a grid.
+    Note: 
+        For overdensity and density contrast, divide by the mean and
+        subtract 1
+
+    Args:
+        pos (np.array): (N, 3) array of particle positions
+        BoxSize (float): size of the box
+        Ngrid (int): number of grid points
+        MAS (str): mass assignment scheme (NGP, CIC, TSC, PCS)
+        verbose (bool, optional): print information on progress
+    """
+    # define 3D density field
+    delta = np.zeros((Ngrid, Ngrid, Ngrid), dtype=np.float32)
+
+    # construct 3D density field
+    MASL.MA(pos, delta, BoxSize, MAS, W=value, verbose=verbose)
+
+    return delta
+
+
+def vfield(ppos, pvel, BoxSize, Ngrid, MAS, omega_m, h, verbose=False):
+    """
+    Measure the 3D velocity field from particles. Also returns the
+    density field.
+
+    Args:
+        ppos (np.array): (N, 3) array of particle positions
+        pvel (np.array): (N, 3) array of particle velocities
+        BoxSize (float): size of the box
+        Ngrid (int): number of grid points
+        MAS (str): mass assignment scheme (NGP, CIC, TSC, PCS)
+        omega_m (float): matter density
+        h (float): Hubble constant
+        verbose (bool, optional): print information on progress
+    """
+    Npart = len(ppos)
+    m_particle = get_particle_mass(Npart, BoxSize, omega_m, h)
+    rho = assign_field(ppos, BoxSize, Ngrid, MAS,
+                       value=m_particle, verbose=verbose)
+
+    pmom = m_particle * pvel
+    mom = np.stack([
+        assign_field(ppos, BoxSize, Ngrid, MAS,
+                     value=pmom[..., i], verbose=verbose)
+        for i in range(3)
+    ], axis=-1)
+
+    vel = mom / rho[..., None]
+    return rho, vel  # TODO: Implement interpolation?
+
+
 @timing_decorator
 def vfield_CIC(ppos, pvel, cfg, interp=True):
+    raise NotImplementedError
+
     nbody = cfg.nbody
     N = nbody.N * nbody.supersampling
     ptcl_spacing = nbody.L / N
