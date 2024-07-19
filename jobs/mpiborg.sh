4638ceaa69a122e798b6a9eb68db253ab242b81f
Matthew Ho <matthew.annam.ho@gmail.com>
2024-07-19 23:00:10 +0200

    adding configuration for 2gpch no snapshot runs

--- jobs/mpiborg.sh
+++ jobs/mpiborg.sh
@@ -2,17 +2,19 @@
 #!/bin/sh
 #PBS -S /bin/sh
 #PBS -N mpiborg
-#PBS -l nodes=2:ppn=64:hasnogpu,mem=128gb
-#PBS -l walltime=12:00:00
+#PBS -l nodes=2:ppn=64,mem=128gb
+#PBS -l walltime=2:00:00
 #PBS -j oe
-#PBS -m be
+#PBS -m a
+#PBS -t 12-1000
 #PBS -o ${HOME}/data/jobout/${PBS_JOBNAME}.${PBS_JOBID}.log
 
 
 module restore myborg
 source /data80/mattho/anaconda3/bin/activate
-conda activate borg310
+conda activate borg310run
 
+# This works for 2gpc/h runs at B=2, ss=3
 NODES=2  # number of physical nodes (same as in nodes hereinabove)
 PPN=2  # (number of MPI tasks per node)
 CORES=16  # (number of cores indicated in ppn hereinabove)
@@ -21,6 +23,6 @@ THREADS=$(($CORES / $PPN))
 TASKS=$(($NODES * $PPN))
 
 # Change directory to adequate workdir
-cd /home/mattho/git/ltu-cmass
-lhid=6
+cd /home/mattho/data/cmass-ili/rundir_cmass
+lhid=$(($PBS_ARRAYID))
 mpirun -genv OMP_NUM_THREADS=$THREADS -genv BORG_TBB_NUM_THREADS=$THREADS -f $PBS_NODEFILE -np $TASKS python -m cmass.nbody.borgpm nbody=test nbody.lhid=$lhid
