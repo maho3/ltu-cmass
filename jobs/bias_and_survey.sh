952856e5259e4035d959d8ef86f15ea4430b5d2f
Matthew Ho <matthew.annam.ho@gmail.com>
2024-07-29 14:14:32 +0200

    add options to bias_and_survey

--- jobs/bias_and_survey.sh
+++ jobs/bias_and_survey.sh
@@ -1,8 +1,8 @@
-#PBS -N bias_survey
+#PBS -N qui_bias_survey
 #PBS -q batch
 #PBS -l walltime=4:00:00
-#PBS -l nodes=1:ppn=32,mem=64gb
-#PBS -t 0-999
+#PBS -l nodes=1:ppn=16,mem=32gb
+#PBS -t 0-99
 #PBS -j oe
 #PBS -m a
 #PBS -o ${HOME}/data/jobout/${PBS_JOBNAME}.${PBS_JOBID}.log
@@ -19,9 +19,35 @@ conda activate cmassrun
 echo running script...
 echo "arrayind is ${PBS_ARRAYID}"
 
-lhid=$(($PBS_ARRAYID))
-python -m cmass.bias.rho_to_halo nbody=test nbody.lhid=$lhid sim=borgpm
-python -m cmass.bias.apply_hod nbody=test nbody.lhid=$lhid sim=borgpm
-python -m cmass.survey.ngc_selection nbody=test nbody.lhid=$lhid sim=borgpm
+nbody=quijotelike
+sim=borgpm
+
+# # FOR INDIVIDUAL RUNS
+# lhid=$(($PBS_ARRAYID))
+# python -m cmass.bias.rho_to_halo nbody=$nbody nbody.lhid=$lhid sim=$sim
+# python -m cmass.bias.apply_hod nbody=$nbody nbody.lhid=$lhid sim=$sim
+# python -m cmass.survey.ngc_selection nbody=$nbody nbody.lhid=$lhid sim=$sim
+
+# # FOR LOOPING AUGMENTATION
+# for i in $(seq 1000 100 1900); do
+#     lhid=$(($i + $PBS_ARRAYID))
+#     python -m cmass.bias.rho_to_halo nbody=$nbody nbody.lhid=$lhid sim=$sim
+#     python -m cmass.bias.apply_hod nbody=$nbody nbody.lhid=$lhid sim=$sim
+#     python -m cmass.survey.ngc_selection nbody=$nbody nbody.lhid=$lhid sim=$sim
+# done
+
+# FOR LOOPING AUGMENTATION WITH HOD
+# PBS_ARRAYID=3
+for i in $(seq 0 100 1900); do
+    lhid=$(($i + $PBS_ARRAYID))
+    python -m cmass.bias.rho_to_halo nbody=$nbody nbody.lhid=$lhid sim=$sim
+    for j in {1..10}; do
+        hod_seed=$(($i/10 + $j))
+        python -m cmass.bias.apply_hod nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed
+        python -m cmass.survey.ngc_selection nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed
+        python -m cmass.diagnostics.summ nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed
+    done
+done
+
 
 echo done
