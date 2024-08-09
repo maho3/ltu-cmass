cc84a7359bec34cba5221db0b9de72044e1897fd
Matthew Ho <matthew.annam.ho@gmail.com>
2024-08-09 14:53:27 +0200

    small bias and survey job updates

--- jobs/bias_and_survey.sh
+++ jobs/bias_and_survey.sh
@@ -1,8 +1,8 @@
-#PBS -N qui_bias_survey
+#PBS -N aba_bias_survey
 #PBS -q batch
 #PBS -l walltime=4:00:00
-#PBS -l nodes=1:ppn=16,mem=32gb
-#PBS -t 0-99
+#PBS -l nodes=1:ppn=32,mem=64gb
+#PBS -t 0-199
 #PBS -j oe
 #PBS -m a
 #PBS -o ${HOME}/data/jobout/${PBS_JOBNAME}.${PBS_JOBID}.log
@@ -19,35 +19,39 @@ conda activate cmassrun
 echo running script...
 echo "arrayind is ${PBS_ARRAYID}"
 
-nbody=quijotelike
+nbody=2gpch_0704
 sim=borgpm
 
-# # FOR INDIVIDUAL RUNS
+# # # FOR INDIVIDUAL RUNS
 # lhid=$(($PBS_ARRAYID))
-# python -m cmass.bias.rho_to_halo nbody=$nbody nbody.lhid=$lhid sim=$sim
-# python -m cmass.bias.apply_hod nbody=$nbody nbody.lhid=$lhid sim=$sim
-# python -m cmass.survey.ngc_selection nbody=$nbody nbody.lhid=$lhid sim=$sim
+# # lhid=131
+# hod_seed=0
+# python -m cmass.bias.rho_to_halo nbody=$nbody nbody.lhid=$lhid sim=$sim meta.cosmofile=./params/abacus_cosmologies.txt
+# python -m cmass.bias.apply_hod nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed meta.cosmofile=./params/abacus_cosmologies.txt
+# # python -m cmass.survey.ngc_selection nbody=$nbody nbody.lhid=$lhid sim=$sim
+# python -m cmass.diagnostics.summ nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed meta.cosmofile=./params/abacus_cosmologies.txt
 
-# # FOR LOOPING AUGMENTATION
-# for i in $(seq 1000 100 1900); do
-#     lhid=$(($i + $PBS_ARRAYID))
-#     python -m cmass.bias.rho_to_halo nbody=$nbody nbody.lhid=$lhid sim=$sim
-#     python -m cmass.bias.apply_hod nbody=$nbody nbody.lhid=$lhid sim=$sim
-#     python -m cmass.survey.ngc_selection nbody=$nbody nbody.lhid=$lhid sim=$sim
-# done
-
-# FOR LOOPING AUGMENTATION WITH HOD
-# PBS_ARRAYID=3
-for i in $(seq 0 100 1900); do
+# FOR LOOPING AUGMENTATION 
+for i in $(seq 0 200 800); do
     lhid=$(($i + $PBS_ARRAYID))
-    python -m cmass.bias.rho_to_halo nbody=$nbody nbody.lhid=$lhid sim=$sim
-    for j in {1..10}; do
-        hod_seed=$(($i/10 + $j))
-        python -m cmass.bias.apply_hod nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed
-        python -m cmass.survey.ngc_selection nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed
-        python -m cmass.diagnostics.summ nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed
-    done
+    # python -m cmass.bias.rho_to_halo nbody=$nbody nbody.lhid=$lhid sim=$sim
+    # python -m cmass.bias.apply_hod nbody=$nbody nbody.lhid=$lhid sim=$sim
+    # python -m cmass.survey.ngc_selection nbody=$nbody nbody.lhid=$lhid sim=$sim
+    python -m cmass.diagnostics.summ nbody=$nbody nbody.lhid=$lhid sim=$sim
 done
+
+# # FOR LOOPING AUGMENTATION WITH HOD
+# # PBS_ARRAYID=3
+# for i in $(seq 0 200 1800); do
+#     lhid=$(($i + $PBS_ARRAYID))
+#     # python -m cmass.bias.rho_to_halo nbody=$nbody nbody.lhid=$lhid sim=$sim
+#     for j in {0..0}; do
+#         hod_seed=$(($i/10 + $j))
+#         python -m cmass.bias.apply_hod nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed
+#         # python -m cmass.survey.ngc_selection nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed
+#         python -m cmass.diagnostics.summ nbody=$nbody nbody.lhid=$lhid sim=$sim bias.hod.seed=$hod_seed
+#     done
+# done
 
 
 echo done
