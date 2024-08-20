28f3d34adeaec8b429372e3f2d219ef257d1c9a6
florpi <florpisoteadadejardin@gmail.com>
2024-08-19 21:37:59 -0400

    first try at halo diagnostics

--- cmass/diagnostics/summ.py
+++ cmass/diagnostics/summ.py
@@ -11,9 +11,12 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 import h5py
 
+from summarizer.data import BoxCatalogue
+import summarizer
+
 from ..utils import get_source_path, timing_decorator
 from ..nbody.tools import parse_nbody_config
-from .tools import MA, MAz, calcPk
+from .tools import MA, MAz, calcPk, get_redshift_space_pos
 
 
 def rho_summ(source_path, L, threads=16, from_scratch=True):
@@ -45,8 +48,87 @@ def rho_summ(source_path, L, threads=16, from_scratch=True):
                 group.create_dataset('Pk', data=Pk)
     return True
 
+def get_box_catalogue(pos, z, L, N):
+    return BoxCatalogue(
+        galaxies_pos=pos,
+        redshift=z,
+        boxsize=L,
+        n_mesh=N,
+    )
 
-def halo_summ(source_path, L, N, h, z, threads=16, from_scratch=True):
+def get_box_catalogue_rsd(pos, vel, z, L, h, axis, N):
+    pos = get_redshift_space_pos(pos=pos, vel=vel, z=z, h=h, axis=axis, L=L, N=N,)
+    return BoxCatalogue(
+        galaxies_pos=pos,
+        redshift=z,
+        boxsize=L,
+        n_mesh=N,
+    )
+
+def get_binning(summary, L, N, threads, rsd=False,):
+    #TODO: make sure using gpu if available
+    ells = [0,] if not rsd else [0,2,4]
+    if summary == 'Pk':
+        return {
+            'n_mesh': N,
+            'los': 'z',
+            'compensation': 'nsc',
+            'ells': ells,
+            'n_threads': threads,
+        }
+    if summary == 'Bk':
+        k_min = 2 * np.pi / L
+        k_max = np.pi * N / L 
+        num_bins = 30
+        return {
+            'k_bins': np.logspace(np.log10(k_min), np.log10(k_max), num_bins),
+            'n_mesh': N,
+            'lmax': 2,
+            'ells': ells,
+        }
+    if summary == 'TwoPCF':
+        num_bins = 60
+        return {
+            'r_bins': np.logspace(-2, np.log10(150.), num_bins),
+            'mu_bins': np.linspace(-1.,1.,201),
+            'ells': ells,
+            'n_threads': threads,
+            'los': 'z',
+        }
+    if summary == 'WST':
+        num_bins = 60
+        return {
+            'J_3d': 3,
+            'L_3d': 3,
+            'integral_powers': [0.8,],
+            'sigma': 0.8,
+            'n_mesh': N,
+        }
+    if summary == 'DensitySplit':
+        num_bins = 60
+        return {
+            'r_bins': np.logspace(-1, np.log10(150.), num_bins),
+            'mu_bins': np.linspace(-1.,1.,201),
+            'n_quantiles': 5,
+            'smoothing_radius': 10.0,
+            'ells': ells,
+            'n_threads': threads,
+            'los': 'z',
+            'n_mesh': N,
+        }
+    if summary == 'kNN':
+        num_bins = 60
+        return {
+            'r_bins': np.logspace(-2, np.log10(30.), num_bins),
+            'k': [1,3,7,11],
+            'n_threads': threads,
+        }
+    else:
+        raise NotImplementedError(f'{summary} not implemented')
+
+
+
+def halo_summ(source_path, L, N, h, z, threads=16, from_scratch=True, summaries=['Pk', 'Bk', 'TwoPCF', 'WST', 'DensitySplit', 'KNN']):
     # check if diagnostics already computed
     outpath = join(source_path, 'diag', 'halos.h5')
     if (not from_scratch) and os.path.isfile(outpath):
@@ -72,25 +154,28 @@ def halo_summ(source_path, L, N, h, z, threads=16, from_scratch=True):
                 hpos = f[a]['pos'][...]
                 hvel = f[a]['vel'][...]
                 hmass = f[a]['mass'][...]
+                # Get summaries in comoving space
+                group = o.create_group(a)
+                box_catalogue = get_box_catalogue(pos=hpos, z=z, L=L, N=N)
+                for summ in summaries:
+                    binning = get_binning(summary, L, N, threads, rsd=False,)
+                    summary = getattr(summarizer, summ)(**binning)(box_catalogue)
+                    for key, value in summary.items():
+                        group.create_dataset(f'{summ}_{key}', data=value)
 
-                # measure halo Pk in comoving space
-                delta = MA(hpos, L, N, MAS='NGP')
-                k, Pk = calcPk(delta, L, MAS='NGP', threads=threads)
-
-                # measure halo Pk in redshift space
-                delta = MAz(hpos, hvel, L, N, h, z, MAS='NGP')
-                kz, Pkz = calcPk(delta, L, MAS='NGP', threads=threads)
+                # Get summaries in redshift space
+                zbox_catalogue = get_box_catalogue(pos=hpos, vel=hvel, h=h, z=z, axis=2, L=L, N=N)
+                for summ in summaries:
+                    binning = get_binning(summary, L, N, threads, rsd=True,)
+                    summary = getattr(summarizer, summ)(**binning)(zbox_catalogue)
+                    for key, value in summary.items():
+                        group.create_dataset(f'{summ}_{key}', data=value)
 
                 # measure halo mass function
                 be = np.linspace(13, 16, 100)
                 hist, _ = np.histogram(hmass, bins=be)
 
                 # Save
-                group = o.create_group(a)
-                group.create_dataset('Pk_k', data=k)
-                group.create_dataset('Pk', data=Pk)
-                group.create_dataset('zPk_k', data=kz)
-                group.create_dataset('zPk', data=Pkz)
                 group.create_dataset('mass_bins', data=be)
                 group.create_dataset('mass_hist', data=hist)
     return True
