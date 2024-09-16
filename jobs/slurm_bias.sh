4245c9318eb14148b466d121deba9e57abad789b
Matthew Ho <matthew.annam.ho@gmail.com>
2024-09-16 11:38:17 -0400

    abacus bias job

--- jobs/slurm_bias.sh
+++ jobs/slurm_bias.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=new_charm   # Job name
 #SBATCH --array=0-999         # Job array range for lhid
 #SBATCH --nodes=1               # Number of nodes
-#SBATCH --ntasks=16            # Number of tasks
-#SBATCH --time=00:15:00         # Time limit
+#SBATCH --ntasks=64            # Number of tasks
+#SBATCH --time=01:00:00         # Time limit
 #SBATCH --partition=shared      # Partition name
 #SBATCH --account=phy240043   # Account name
 #SBATCH --output=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out  # Output file for each array task
@@ -13,13 +13,12 @@ module restore cmass
 source /anvil/projects/x-phy240043/x-mho1/anaconda3/bin/activate
 conda activate cmass
 lhid=$SLURM_ARRAY_TASK_ID
-# lhid=3
 
 # Command to run for each lhid
 cd /home/x-mho1/git/ltu-cmass
 # 0-1000
-python -m cmass.bias.rho_to_halo nbody=quijotelike sim=fastpm nbody.lhid=$lhid
+python -m cmass.bias.rho_to_halo nbody=abacuslike sim=fastpm nbody.lhid=$lhid
 
 # 1000-2000
 lhid=$((lhid+1000))
-python -m cmass.bias.rho_to_halo nbody=quijotelike sim=fastpm nbody.lhid=$lhid
+python -m cmass.bias.rho_to_halo nbody=abacuslike sim=fastpm nbody.lhid=$lhid
