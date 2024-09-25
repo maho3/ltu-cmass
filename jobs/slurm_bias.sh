32f289eba1b80f2c5eafc5362b633fad112edad9
Matthew Ho <matthew.annam.ho@gmail.com>
2024-09-25 09:49:08 -0400

    adding scripts and configurations for recent suite runs

--- jobs/slurm_bias.sh
+++ jobs/slurm_bias.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=new_charm   # Job name
+#SBATCH --job-name=mtng_bias   # Job name
 #SBATCH --array=0-999         # Job array range for lhid
 #SBATCH --nodes=1               # Number of nodes
-#SBATCH --ntasks=64            # Number of tasks
-#SBATCH --time=01:00:00         # Time limit
+#SBATCH --ntasks=32            # Number of tasks
+#SBATCH --time=03:00:00         # Time limit
 #SBATCH --partition=shared      # Partition name
 #SBATCH --account=phy240043   # Account name
 #SBATCH --output=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out  # Output file for each array task
@@ -14,11 +14,60 @@ source /anvil/projects/x-phy240043/x-mho1/anaconda3/bin/activate
 conda activate cmass
 lhid=$SLURM_ARRAY_TASK_ID
 
+outdir=/anvil/scratch/x-mho1/cmass-ili/mtnglike/fastpm/L3000-N384
+
 # Command to run for each lhid
 cd /home/x-mho1/git/ltu-cmass
-# 0-1000
-python -m cmass.bias.rho_to_halo nbody=abacuslike sim=fastpm nbody.lhid=$lhid
 
-# 1000-2000
-lhid=$((lhid+1000))
-python -m cmass.bias.rho_to_halo nbody=abacuslike sim=fastpm nbody.lhid=$lhid
+Nhod=10
+Naug=5
+
+
+for offset in 0 1000; do
+    lhid=$(($SLURM_ARRAY_TASK_ID+offset))
+
+    postfix="nbody=mtnglike sim=fastpm nbody.zf=0.500015 nbody.lhid=$lhid"
+
+    # halos
+    file=$outdir/$lhid/halos.h5
+    if [ -f $file ]; then
+        echo "File $file exists."
+    else
+        echo "File $file does not exist."
+        python -m cmass.bias.rho_to_halo $postfix
+    fi
+    # galaxies
+    for i in $(seq 0 $Nhod); do
+        hod_seed=$((lhid*10+i+1))
+        printf -v hod_str "%05d" $hod_seed
+        file=$outdir/$lhid/galaxies/hod$hod_str.h5
+        if [ -f $file ]; then
+            echo "File $file exists."
+        else
+            echo "File $file does not exist."
+            python -m cmass.bias.apply_hod $postfix bias.hod.seed=$hod_seed
+        fi
+
+        # augments
+        for aug_seed in $(seq 0 $Naug); do
+            printf -v aug_str "%05d" $aug_seed
+            # lightcone
+            file=$outdir/$lhid/lightcone/hod${hod_str}_aug${aug_str}.h5
+            if [ -f $file ]; then
+                echo "File $file exists."
+            else
+                echo "File $file does not exist."
+                python -m cmass.survey.ngc_lightcone $postfix bias.hod.seed=$hod_seed survey.aug_seed=$aug_seed
+            fi
+
+            # diagnostics
+            file=$outdir/$lhid/diag/lightcone/hod${hod_str}_aug${aug_str}.h5
+            if [ -f $file ]; then
+                echo "File $file exists."
+            else
+                echo "File $file does not exist."
+                python -m cmass.diagnostics.summ_lc $postfix bias.hod.seed=$hod_seed survey.aug_seed=$aug_seed
+            fi
+        done
+    done
+done
--- jobs/slurm_summ.sh
+++ jobs/slurm_summ.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=a=quifsumm   # Job name
+#SBATCH --job-name=summ   # Job name
 #SBATCH --array=0-999         # Job array range for lhid
 #SBATCH --nodes=1               # Number of nodes
-#SBATCH --ntasks=32            # Number of tasks
-#SBATCH --time=01:00:00         # Time limit
+#SBATCH --ntasks=8            # Number of tasks
+#SBATCH --time=03:00:00         # Time limit
 #SBATCH --partition=shared      # Partition name
 #SBATCH --account=phy240043   # Account name
 #SBATCH --output=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out  # Output file for each array task
@@ -12,13 +12,18 @@
 module restore cmass
 source /anvil/projects/x-phy240043/x-mho1/anaconda3/bin/activate
 conda activate cmass
-lhid=$SLURM_ARRAY_TASK_ID
+# lhid=$SLURM_ARRAY_TASK_ID
+# SLURM_ARRAY_TASK_ID=3
+
 
 # Command to run for each lhid
 cd /home/x-mho1/git/ltu-cmass
-# 0-1000
-python -m cmass.diagnostics.summ nbody=quijotelike sim=fastpm nbody.lhid=$lhid
 
-# 1000-2000
-lhid=$((lhid+1000))
-python -m cmass.diagnostics.summ nbody=quijotelike sim=fastpm nbody.lhid=$lhid
+
+for offset in 0 1000; do
+    lhid=$(($SLURM_ARRAY_TASK_ID+$offset))
+
+    postfix="nbody=abacuslike sim=fastpm nbody.zf=0.500015 nbody.lhid=$lhid"
+
+    python -m cmass.diagnostics.summ $postfix
+done
