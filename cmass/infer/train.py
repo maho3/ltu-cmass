a1f93e7c39ccbc0e10d767a1afc976eb0154bb91
Matthew Ho <matthew.annam.ho@gmail.com>
2024-11-27 03:31:33 -0500

    it works!

--- cmass/infer/train.py
+++ cmass/infer/train.py
@@ -10,6 +10,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 import h5py
 from collections import defaultdict
+from tqdm import tqdm
 
 from ..utils import get_source_path, timing_decorator
 from ..nbody.tools import parse_nbody_config
@@ -20,7 +21,8 @@ from ili.dataloaders import NumpyLoader
 from ili.inference import InferenceRunner
 from ili.validation.metrics import PosteriorCoverage, PlotSinglePosterior
 from ili.embedding import FCN
-device='cpu'
+
+import matplotlib.pyplot as plt
 
 
 def get_cosmo(source_path):
@@ -29,14 +31,22 @@ def get_cosmo(source_path):
 
 def get_halo_Pk(source_path, a):
     diag_file = join(source_path, 'diag', 'halos.h5')
+    a = f'{a:.6f}'
     if not os.path.exists(diag_file):
         return {}
     
     summ = {}
-    with h5py.File(diag_file, 'r') as f:
-        for key in ['Pk_k', 'Pk', 'zPk_k', 'zPk']:
-            if key in f[a]:  # save summary if its available
-                summ[key] = f[a][key][:]
+    try:
+        with h5py.File(diag_file, 'r') as f:
+            for stat in ['Pk', 'zPk']:
+                if stat in f[a]:
+                    summ[stat] = f[a][stat][:]
+                    if stat+'_k' in f[a]:  # backwards compatibility  (todo: remove)
+                        summ[stat+'_k'] = f[a][stat+'_k'][:]
+                    elif stat+'_k3D' in f[a]:
+                        summ[stat+'_k'] = f[a][stat+'_k3D'][:]
+    except OSError:
+        return {}
     summ['cosmo'] = get_cosmo(source_path)
     return summ
 
@@ -44,8 +54,9 @@ def load_halo_summaries(suitepath, a):
     logging.info(f'Looking for halo summaries at {suitepath}')
 
     simpaths = os.listdir(suitepath)
+    # simpaths = simpaths[:100]  # for testing
     summaries, parameters, meta = defaultdict(list), defaultdict(list), defaultdict(list)
-    for lhid in simpaths:
+    for lhid in tqdm(simpaths):
         summ = get_halo_Pk(join(suitepath, lhid), a)
         for key in ['Pk', 'zPk']:
             if key in summ:
@@ -63,6 +74,123 @@ def load_halo_summaries(suitepath, a):
     return summaries, parameters, meta
 
 
+def cut_kmax(x, k, kmax):
+    if kmax is None:
+        return x
+    if len(k.shape) > 1:
+        k = k[0]
+    mask = k < kmax
+    return x[:, mask, ...]
+
+
+def preprocess(x, Pk_poles):
+    # only use desired poles
+    x = x[..., Pk_poles]
+
+    # log transform
+    x = np.log(x + 1e-5)
+
+    # impute nans
+    x = np.nan_to_num(x, nan=0.0)
+
+    # flatten
+    x = x.reshape(len(x), -1)
+
+    return x
+
+def split_train_test(x, theta, test_frac):
+    cutoff = int(len(x) * (1 - test_frac))
+    x_train, x_test = x[:cutoff], x[cutoff:]
+    theta_train, theta_test = theta[:cutoff], theta[cutoff:]
+    return x_train, x_test, theta_train, theta_test
+
+def run_inference(x, theta, cfg, out_dir):
+    loader = NumpyLoader(x=x, theta=theta)
+
+    # define a prior
+    if cfg.infer.prior.lower() == 'uniform':
+        prior = ili.utils.Uniform(
+            low=theta.min(axis=0),
+            high=theta.max(axis=0),
+            device=cfg.infer.device)
+    else:
+        raise NotImplementedError
+
+    embedding = FCN(
+        n_hidden=cfg.infer.fcn_hidden, 
+        act_fn='ReLU'
+    )
+
+    # instantiate your neural networks to be used as an ensemble
+    if cfg.infer.backend == 'lampe':
+        net_loader = ili.utils.load_nde_lampe
+    elif cfg.infer.backend == 'sbi':
+        net_loader = ili.utils.load_nde_sbi
+    else:
+        raise NotImplementedError
+    nets = [
+        net_loader(
+            model=net.model, hidden_features=net.hidden_features,
+            num_transforms=net.num_transforms,
+            embedding_net=embedding)
+        for net in cfg.infer.nets
+    ]
+
+    # define training arguments
+    train_args = {
+        'training_batch_size': cfg.infer.batch_size,
+        'learning_rate': cfg.infer.learning_rate,
+        'validation_fraction': cfg.infer.val_frac,
+    }
+
+    # make output directory
+    os.makedirs(out_dir, exist_ok=True)
+
+    # initialize the trainer
+    runner = InferenceRunner.load(
+        backend=cfg.infer.backend,
+        engine=cfg.infer.engine,
+        prior=prior,
+        nets=nets,
+        device=cfg.infer.device,
+        train_args=train_args,
+        out_dir=out_dir
+    )
+
+    # train the model
+    posterior, histories = runner(loader=loader)
+
+    return posterior, histories
+
+def run_validation(posterior, history, x, theta, out_dir):
+    logging.info('Running validation...')
+
+    # Plot training history
+    f, ax = plt.subplots(1, 1, figsize=(6, 4))
+    for i, h in enumerate(history):
+        ax.plot(h['validation_log_probs'], label=f'Net {i}')
+    ax.set(xlabel='Epoch', ylabel='Validation log prob')
+    ax.legend()
+    f.savefig(join(out_dir, 'loss.jpg'), dpi=200, bbox_inches='tight')
+
+
+    # Plotting single posterior
+    xobs, thetaobs = x[0], theta[0]
+    metric = PlotSinglePosterior(
+        num_samples=1000, sample_method='direct',
+        labels=['Omega_m', 'Omega_b', 'h', 'n_s', 'sigma8']
+    )
+    metric(posterior, x_obs=xobs, theta_fid=thetaobs)
+
+    # Posterior coverage
+    metric = PosteriorCoverage(
+        num_samples=1000, sample_method='direct', 
+        labels=['Omega_m', 'Omega_b', 'h', 'n_s', 'sigma8'],
+        plot_list = ["coverage", "histogram", "predictions", "tarp", "logprob"],
+        out_dir=out_dir
+    )
+    metric(posterior, x, theta)
+
 
 @timing_decorator
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
@@ -74,11 +202,27 @@ def main(cfg: DictConfig) -> None:
     suite_path = get_source_path(
         cfg.meta.wdir, cfg.nbody.suite, cfg.sim,
         cfg.nbody.L, cfg.nbody.N, 0, check=False
-    )[:-2]  # remove the last directory
+    )[:-2]  # get to the suite directory
+    model_path = join(cfg.meta.wdir, cfg.nbody.suite, cfg.sim, 'models')
 
     if cfg.infer.halo:
-        summaries, parameters, meta = load_halo_summaries(suite_path, cfg.infer.a)
-        
+        logging.info('Running halo inference...')
+        summaries, parameters, meta = load_halo_summaries(suite_path, cfg.nbody.af)
+        for key in summaries:
+            for kmax in cfg.infer.kmax:
+                logging.info(f'Running inference for {key} with kmax={kmax}')
+                out_dir = join(model_path, 'halos', f'{key}', f'kmax-{kmax}')
+                x, theta = summaries[key], parameters[key]
+                x = cut_kmax(x, meta[key], kmax)
+                x = preprocess(x, cfg.infer.Pk_poles)
+                x_train, x_test, theta_train, theta_test = \
+                    split_train_test(x, theta, cfg.infer.test_frac)
+
+                posterior, history = run_inference(x_train, theta_train, cfg, out_dir)
+
+                run_validation(posterior, history, x_test, theta_test, out_dir)
+    else:
+        logging.info('Skipping halo inference...')
 
 
 if __name__ == "__main__":
