c319ab58d6cff82b02c038d3b94b6383805be62e
Matthew Ho <matthew.annam.ho@gmail.com>
2024-12-22 17:59:46 -0500

    slightly preferred inference settings

--- jobs/slurm_inference.sh
+++ jobs/slurm_inference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=inference  # Job name
-#SBATCH --array=0-12  # Array range
+#SBATCH --array=0-15  # Array range
 #SBATCH --nodes=1               # Number of nodes
 #SBATCH --ntasks=32            # Number of tasks
 #SBATCH --gpus-per-node=1     # Number of GPUs
@@ -11,26 +11,29 @@
 #SBATCH --error=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out   # Error file for each array task
 
 # SLURM_ARRAY_TASK_ID=1
+export TQDM_DISABLE=0
 
 module restore cmass
 conda activate cmassrun
-export TQDM_DISABLE=0
 
 exp_index=$SLURM_ARRAY_TASK_ID
 
 # Command to run for each lhid
 cd /home/x-mho1/git/ltu-cmass-run
 
-nbody=abacuslike
+nbody=quijotelike
 sim=fastpm
-halo=False
-galaxy=False
+halo=True
+galaxy=True
 ngc=False
-sgc=True
-extras="nbody.zf=0.500015"
+sgc=False
+extras="" # "nbody.zf=0.500015"
 device=cuda
 
-postfix="nbody=$nbody sim=$sim infer.exp_index=$exp_index infer.halo=$halo infer.galaxy=$galaxy infer.ngc_lightcone=$ngc infer.sgc_lightcone=$sgc infer.device=$device $extras"
+postfix="nbody=$nbody sim=$sim infer.exp_index=$exp_index"
+postfix="$postfix infer.halo=$halo infer.galaxy=$galaxy"
+postfix="$postfix infer.ngc_lightcone=$ngc infer.sgc_lightcone=$sgc"
+postfix="$postfix infer.device=$device $extras"
 
 echo "Running inference with $postfix"
 python -m cmass.infer.train $postfix
