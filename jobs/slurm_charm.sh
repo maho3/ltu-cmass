493befc4da5172e4830db4726b165525d7fcb970
Matthew Ho <matthew.annam.ho@gmail.com>
2025-01-06 14:11:14 -0500

    keychecking in slurm_charm

--- jobs/slurm_charm.sh
+++ jobs/slurm_charm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=quijotelike  # Job name
+#SBATCH --job-name=charm_mtng  # Job name
 #SBATCH --array=0-199         # Job array range for lhid
 #SBATCH --nodes=1               # Number of nodes
-#SBATCH --ntasks=32            # Number of tasks
+#SBATCH --ntasks=8            # Number of tasks
 #SBATCH --time=12:00:00         # Time limit
 #SBATCH --partition=gpu        # Partition name
 #SBATCH --gpus-per-node=1       # Number of GPUs per node
@@ -10,37 +10,56 @@
 #SBATCH --output=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out  # Output file for each array task
 #SBATCH --error=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out   # Error file for each array task
 
-# SLURM_ARRAY_TASK_ID=199
+# SLURM_ARRAY_TASK_ID=50
 echo "SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID"
-baseoffset=2000
+baseoffset=0
 
 module restore cmass
-source /anvil/scratch/x-mho1/anaconda3/bin/activate
 conda activate cmassrun
 lhid=$((SLURM_ARRAY_TASK_ID + baseoffset))
 
 # Command to run for each lhid
 cd /home/x-mho1/git/ltu-cmass-run
 
-nbody=quijotelike
+nbody=mtnglike
 sim=fastpm
-extras="nbody.matchIC=0 meta.cosmofile=./params/big_sobol_params.txt"
-L=1000
-N=128
+extras="" # "nbody.matchIC=0 meta.cosmofile=./params/big_sobol_params.txt"
+L=3000
+N=384
+keys_to_check=(0.586220 0.606330 0.626440 0.646550 0.666660 0.686770 0.706880 0.726990 0.747100 0.767210)
 
 outdir=/anvil/scratch/x-mho1/cmass-ili/$nbody/$sim/L$L-N$N
 echo "outdir=$outdir"
 
+# Loop through offsets and process files
 for offset in $(seq 0 200 1800); do
     loff=$((lhid + offset))
-    
     postfix="nbody=$nbody sim=$sim nbody.lhid=$loff $extras"
-
     file=$outdir/$loff/halos.h5
+
+    # Check if file exists
     if [ -f $file ]; then
         echo "File $file exists."
+        all_keys_exist=true
+
+        # Check if all keys exist in the file
+        for key in $keys_to_check; do
+            if ! h5ls $file | grep -q $key; then
+                all_keys_exist=false
+                echo "Key $key does not exist in $file."
+                break
+            fi
+        done
+
+        # Process file based on key existence
+        if $all_keys_exist; then
+            echo "All keys exist in $file. Skipping..."
+        else
+            echo "Not all keys exist in $file. Rerunning..."
+            python -m cmass.bias.rho_to_halo $postfix
+        fi
     else
-        echo "File $file does not exist."
+        echo "File $file does not exist. Running..."
         python -m cmass.bias.rho_to_halo $postfix
     fi
 done
