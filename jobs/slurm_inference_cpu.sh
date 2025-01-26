42a6017716cefc7208e0ebd86657f51f93469768
Matthew Ho <matthew.annam.ho@gmail.com>
2025-01-26 17:01:35 -0500

    update inference

--- jobs/slurm_inference_cpu.sh
+++ jobs/slurm_inference_cpu.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=inference  # Job name
-#SBATCH --array=0-23  # Array range
+#SBATCH --array=0-9  # Array range
 #SBATCH --nodes=1               # Number of nodes
 #SBATCH --ntasks=32            # Number of tasks
-#SBATCH --time=1:00:00         # Time limit
+#SBATCH --time=12:00:00         # Time limit
 #SBATCH --partition=shared  # Partition name
 #SBATCH --account=phy240043  # Account name
 #SBATCH --output=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out  # Output file for each array task
 #SBATCH --error=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out   # Error file for each array task
 
-# SLURM_ARRAY_TASK_ID=null
+# SLURM_ARRAY_TASK_ID=6
 export TQDM_DISABLE=0
 
 module restore cmass
-conda activate cmass
+conda activate cmassrun
 
 exp_index=$SLURM_ARRAY_TASK_ID
 
 # Command to run for each lhid
-cd /home/x-mho1/git/ltu-cmass
+cd /home/x-mho1/git/ltu-cmass-run
 
 nbody=mtnglike
 sim=fastpm
@@ -39,5 +39,5 @@ postfix="$postfix infer.ngc_lightcone=$ngc infer.sgc_lightcone=$sgc infer.mtng_l
 postfix="$postfix infer.device=$device $extras"
 
 echo "Running inference with $postfix"
-python -m cmass.infer.preprocess $postfix
+# python -m cmass.infer.preprocess $postfix
 python -m cmass.infer.train $postfix
