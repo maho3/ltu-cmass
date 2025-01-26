393dbc2290620f858a6cb5cf5d607e9fae4d5f82
Matthew Ho <matthew.annam.ho@gmail.com>
2025-01-26 17:05:21 -0500

    running more large volume mtng sims

--- jobs/slurm_mtng_highmem.sh
+++ jobs/slurm_mtng_highmem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=mtnglike_highm   # Job name
-#SBATCH --array=1         # Job array range for lhid
-#SBATCH --time=00:60:00         # Time limit
+#SBATCH --array=1-3         # Job array range for lhid
+#SBATCH --time=02:00:00         # Time limit
 #SBATCH --account=phy240043   # Account name
 #SBATCH --output=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out  # Output file for each array task
 #SBATCH --error=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out   # Error file for each array task
@@ -9,11 +9,12 @@
 #SBATCH --ntasks=128            # Number of tasks
 #SBATCH --partition=highmem      # Partition name
 
+# SLURM_ARRAY_TASK_ID=1
+offset=2000
+
 module restore cmass
-module load openmpi
-source /anvil/projects/x-phy240043/x-mho1/anaconda3/bin/activate
 conda activate cmassrun
-lhid=$SLURM_ARRAY_TASK_ID
+lhid=$((SLURM_ARRAY_TASK_ID + offset))
 
 # Command to run for each lhid
 cd /home/x-mho1/git/ltu-cmass-run
--- jobs/slurm_mtng_whole.sh
+++ jobs/slurm_mtng_whole.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=mtnglike_whole   # Job name
-#SBATCH --array=766-999         # Job array range for lhid
+#SBATCH --array=11-100        # Job array range for lhid
 #SBATCH --time=06:00:00         # Time limit
 #SBATCH --account=phy240043   # Account name
 #SBATCH --output=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out  # Output file for each array task
@@ -9,24 +9,33 @@
 #SBATCH --ntasks=896            # Number of tasks
 #SBATCH --partition=wholenode      # Partition name
 
+# SLURM_ARRAY_TASK_ID=4
+offset=2000
+
 module restore cmass
-module load openmpi
-source /anvil/projects/x-phy240043/x-mho1/anaconda3/bin/activate
 conda activate cmassrun
-lhid=$SLURM_ARRAY_TASK_ID
-# lhid=432
-# lhid=766
+lhid=$((SLURM_ARRAY_TASK_ID + offset))
 
 # Command to run for each lhid
 cd /home/x-mho1/git/ltu-cmass-run
-# outdir=/anvil/scratch/x-mho1/cmass-ili/mtnglike/fastpm/L3000-N384
+outdir=/anvil/scratch/x-mho1/cmass-ili/mtnglike/fastpm/L3000-N384
 
 # 0-1000
-python -m cmass.nbody.fastpm nbody=mtnglike nbody.lhid=$lhid
-# sbatch --export=lhid=$lhid jobs/slurm_mtng_postprocess.sh
+# check if nbody.h5 exists
+file=$outdir/$lhid/nbody.h5
+if [ -f $file ]; then
+    echo "File $file exists."
+else
+    echo "File $file does not exist."
+    python -m cmass.nbody.fastpm nbody=mtnglike nbody.lhid=$lhid
+fi
 
-# 1000-2000
-lhid=$((lhid+1000))
-python -m cmass.nbody.fastpm nbody=mtnglike nbody.lhid=$lhid
-# sbatch --export=lhid=$lhid jobs/slurm_mtng_postprocess.sh
-
+# # 1000-2000
+# lhid=$((lhid+1000))
+# file=$outdir/$lhid/nbody.h5
+# if [ -f $file ]; then
+#     echo "File $file exists."
+# else
+#     echo "File $file does not exist."
+#     python -m cmass.nbody.fastpm nbody=mtnglike nbody.lhid=$lhid
+# fi
