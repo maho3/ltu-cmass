43910f11ba8f6aa3604d3fc26ea20b9b73bbd0e5
Matthew Ho <mho1@br014.ib.bridges2.psc.edu>
2025-01-28 17:46:09 -0500

    bridges customization

--- jobs/slurm_inference.sh
+++ jobs/slurm_inference.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=inference  # Job name
-#SBATCH --array=0-15  # Array range
+#SBATCH --job-name=sgcinference  # Job name
+#SBATCH --array=0-23  # Array range
 #SBATCH --nodes=1               # Number of nodes
-#SBATCH --ntasks=32            # Number of tasks
-#SBATCH --gpus-per-node=1     # Number of GPUs
+#SBATCH --ntasks=5            # Number of tasks
+#SBATCH --gres=gpu:1            # Number of GPUs
 #SBATCH --time=12:00:00         # Time limit
-#SBATCH --partition=gpu # Partition name
-#SBATCH --account=phy240043-gpu   # Account name
-#SBATCH --output=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out  # Output file for each array task
-#SBATCH --error=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out   # Error file for each array task
+#SBATCH --partition=GPU-shared # Partition name
+#SBATCH --account=phy240015p   # Account name
+#SBATCH --output=/ocean/projects/phy240015p/mho1/jobout/%x_%A_%a.out  # Output file for each array task
+#SBATCH --error=/ocean/projects/phy240015p/mho1/jobout/%x_%A_%a.out   # Error file for each array task
 
-SLURM_ARRAY_TASK_ID=3
-# export TQDM_DISABLE=0
+# SLURM_ARRAY_TASK_ID=null
+export TQDM_DISABLE=0
 
 module restore cmass
 conda activate cmass
@@ -19,26 +19,26 @@ conda activate cmass
 exp_index=$SLURM_ARRAY_TASK_ID
 
 # Command to run for each lhid
-cd /home/x-mho1/git/ltu-cmass
+cd /jet/home/mho1/git/ltu-cmass
 
-nbody=mtnglike
+nbody=abacuslike
 sim=fastpm
 infer=default
 
 halo=False
 galaxy=False
-ngc=True
-sgc=False
+ngc=False
+sgc=True
 mtng=False
 
 extras="nbody.zf=0.500015"
 device=cuda
 
-postfix="nbody=$nbody sim=$sim infer=$infer infer.exp_index=$exp_index"
-postfix="$postfix infer.halo=$halo infer.galaxy=$galaxy"
-postfix="$postfix infer.ngc_lightcone=$ngc infer.sgc_lightcone=$sgc infer.mtng_lightcone=$mtng"
-postfix="$postfix infer.device=$device $extras"
+suffix="nbody=$nbody sim=$sim infer=$infer infer.exp_index=$exp_index"
+suffix="$suffix infer.halo=$halo infer.galaxy=$galaxy"
+suffix="$suffix infer.ngc_lightcone=$ngc infer.sgc_lightcone=$sgc infer.mtng_lightcone=$mtng"
+suffix="$suffix infer.device=$device $extras"
 
-echo "Running inference with $postfix"
-python -m cmass.infer.preprocess $postfix
-python -m cmass.infer.train $postfix
+echo "Running inference with $suffix"
+# python -m cmass.infer.preprocess $suffix
+python -m cmass.infer.train $suffix
