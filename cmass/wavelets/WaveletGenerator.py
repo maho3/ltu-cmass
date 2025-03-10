d0a6f5be08421ccb8ccbf5a6c0803658797d0654
Anirban Bairagi <anirbanbairagi8@gmail.com>
2025-03-10 15:54:16 -0400

    Wavelet for all realizations

--- cmass/wavelets/WaveletGenerator.py
+++ cmass/wavelets/WaveletGenerator.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd 
 import argparse, math, os, sys, glob, multiprocessing
 import torch
+import hydra
+from omegaconf import DictConfig
 
 from kymatio.torch import HarmonicScattering3D
 from kymatio.scattering3d.backend.torch_backend \
@@ -10,7 +12,9 @@ from kymatio.caching import get_cache_dir
 from cmass.utils import get_source_path, timing_decorator
 from cmass.diagnostics.tools import MA
 from cmass.infer.loaders import get_cosmo, get_hod 
-import glob
+from cmass.nbody.tools import parse_nbody_config
+from cmass.bias.apply_hod import parse_hod
+import glob, h5py
 
 cosmo_params=["Omega_m", "Omega_b", "h", "n_s", "sigma_8"]
 hod_params=['logMmin', 'sigma_logM', 'logM0', 'logM1', 'alpha']
@@ -37,13 +41,15 @@ def divide_into_batches(X, batch_size = 64):
     
     return mini_batches
 
-def Wavelets(dataset, out_dir, L, N, batchsize=128):
+def Wavelets(dataset, out_dir, L, N, batchsize, tracer):
+    
+    
     # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     # The actual maximum width of the dilated wavelet is 2*sigma*2^J=128, when the pixels in a box side is 128;
     sigma = 1.0
     J = 6
     # Maximum angular frequency of harmonics: L=4;
-    L = 6
+    l = 6
     # The list of integral powers, which is the power applied after the modulus operation, i.e. Sum |f*g|^{integral_powers}
     integral_powers = [0.5, 1.0, 2.0, 3.0, 4.0]
     # Up to 2nd-order coefficient;
@@ -51,14 +57,15 @@ def Wavelets(dataset, out_dir, L, N, batchsize=128):
 
     # Initialize the scattering calculation;
     scattering = HarmonicScattering3D(J=J, shape=(N, N, N),
-                                      L=L, sigma_0=sigma,max_order=max_order,
+                                      L=l, sigma_0=sigma,max_order=max_order,
                                       integral_powers=integral_powers)
     # To cuda;
     scattering.cuda()
 
-    WPH=[]
-    COSMO=np.array([]).reshape(0,5)
-    HOD=np.array([]).reshape(0,5)
+    WPH = []
+    COSMO = np.array([]).reshape(0,5)
+    if tracer == "galaxies":
+        HOD = np.array([]).reshape(0,5)
 
     ## Loop over the simulated boxes to calculate the coefficients;
     # Batch size of the calculation at a time;
@@ -77,18 +84,20 @@ def Wavelets(dataset, out_dir, L, N, batchsize=128):
         # get_cosmo
         source_list = [file.split('/') for file in datafile]
         q = [file.pop(-1) for file in source_list]
-        q = [file.pop(-1) for file in source_list]
+        if tracer == "galaxies":
+            q = [file.pop(-1) for file in source_list]
         source_list = [os.path.join('/', *file) for file in source_list]
         cosmo = [get_cosmo(source_path) for source_path in source_list]
         COSMO = np.vstack([COSMO,cosmo])
         
         # get_hod
-        diagfile_list = [file.split('/') for file in datafile]
-        q = [file.insert(-2,'diag') for file in diagfile_list]
-        diagfile_list = [os.path.join('/', *file) for file in diagfile_list]
-        hod = [get_hod(diagfile) for diagfile in diagfile_list]
-        HOD=np.vstack([HOD,hod])
-               
+        if tracer == "galaxies":
+            diagfile_list = [file.split('/') for file in datafile]
+            q = [file.insert(-2,'diag') for file in diagfile_list]
+            diagfile_list = [os.path.join('/', *file) for file in diagfile_list]
+            hod = [get_hod(diagfile) for diagfile in diagfile_list]
+            HOD=np.vstack([HOD,hod])
+
         
         x=np.array(batch_x)
         x=torch.from_numpy(x)
@@ -103,13 +112,18 @@ def Wavelets(dataset, out_dir, L, N, batchsize=128):
         order0=order0_gpu.cpu().numpy()
 
 
-        for i in np.arange(len(order12)):        
+        for i in np.arange(len(datafile)):       
 
-            # 1st and 2nd;
-            filename = os.path.join(out_dir, datafile[i].split('/')[-3],'/diag/galaxies/WST/')
-            if not os.path.exists(filename):
-                os.makedirs(filename, exist_ok=True)
-                
+            if tracer == "nbody" or tracer == "halos":
+                filename = os.path.join(out_dir, datafile[i].split('/')[-2],'diag/WST/')
+                if not os.path.exists(filename):
+                    os.makedirs(filename, exist_ok=True)
+            
+            if tracer == "galaxies":
+                filename = os.path.join(out_dir, datafile[i].split('/')[-3],'diag/galaxies/WST/')
+                if not os.path.exists(filename):
+                    os.makedirs(filename, exist_ok=True)
+
             filename12 = os.path.join(filename, 'S12_J6_L6_q0.5/')
             if not os.path.exists(filename12):
                 os.mkdir(filename12)
@@ -127,34 +141,59 @@ def Wavelets(dataset, out_dir, L, N, batchsize=128):
 
             WPH.append([filename0, filename])
 
-    return WPH, COSMO, HOD
+    if tracer == "galaxies":
+        return WPH, COSMO, HOD
+    else:
+        return WPH, COSMO
 
-
+@hydra.main(version_base=None, config_path="../conf", config_name="config")
 def main(cfg: DictConfig) -> None:
-    # cfg = parse_nbody_config(cfg)
-    # cfg = parse_hod(cfg)
+    print(os.getcwd())
+    cfg = parse_nbody_config(cfg)
+    cfg = parse_hod(cfg)
     
     source_path = get_source_path(
         cfg.meta.wdir, cfg.nbody.suite, cfg.sim,
-        cfg.nbody.L, cfg.nbody.N, str('*')
+        cfg.nbody.L, cfg.nbody.N, str('*'), check=False
     )
     
-    dataset=glob.glob(source_path+'galaxies/*.h5')   # change for other tracers
+    if cfg.diag.density:
+        dataset = glob.glob(source_path+'/nbody.h5') 
+        tracer = "nbody"
+    
+    elif cfg.diag.halo:
+        dataset = glob.glob(source_path+'/halos.h5') 
+        tracer = "halos"
         
+    elif cfg.diag.galaxy:
+        dataset = glob.glob(source_path+'/galaxies/*.h5')  
+        tracer = "galaxies"
+    
     # output directory to store wavelets
-    l = source_path.split('/')
-    q = l.pop(-1)
-    out_dir = os.path.join('/', *l)
-        
-    WPH, COSMO, HOD = Wavelets(dataset, out_dir, cfg.nbody.L, cfg.nbody.N, batchsize=128)
-    dataframe=pd.DataFrame(columns=["S0", "S12", "Omega_m", "Omega_b", "h", "n_s", "sigma_8", 
+    str_list = source_path.split('/')
+    q = str_list.pop(-1)
+    out_dir = os.path.join('/', *str_list)
+    
+    if cfg.diag.galaxy:
+        WPH, COSMO, HOD = Wavelets(dataset, out_dir, cfg.nbody.L, cfg.nbody.N, cfg.diag.batchsize, tracer)
+        dataframe = pd.DataFrame(columns=["S0", "S12", "Omega_m", "Omega_b", "h", "n_s", "sigma_8", 
                                         'logMmin', 'sigma_logM', 'logM0', 'logM1', 'alpha'])
-    dataframe[["S0","S12"]]=WPH        
-    dataframe[cosmo_params]=COSMO  
-    dataframe[hod_params]=hod  
+        dataframe[["S0","S12"]]= WPH        
+        dataframe[cosmo_params] = COSMO 
+        dataframe[hod_params] = hod 
+    
+    else:
+        WPH, COSMO = Wavelets(dataset, out_dir, cfg.nbody.L, cfg.nbody.N, cfg.diag.batchsize, tracer)
+        dataframe = pd.DataFrame(columns=["S0", "S12", "Omega_m", "Omega_b", "h", "n_s", "sigma_8"])
+        print(WPH.shape)
+        dataframe[["S0","S12"]] = WPH        
+        dataframe[cosmo_params] = COSMO  
+     
         
-        # Output_dir_csv
-    out_csv=os.path.join("Dataset", cfg.nbody.suite, cfg.sim, f'L{L}-N{N}')
+    # Output_dir_csv
+    out_csv=os.path.join("cmass/wavelets/Dataset", cfg.nbody.suite, cfg.sim, f'L{cfg.nbody.L}-N{cfg.nbody.N}')
     os.makedirs(out_csv, exist_ok=True)
-    dataframe.to_csv(out_csv+"/"+"wst_q0.5.csv")
+    dataframe.to_csv(out_csv+"/"+tracer+".csv")
         
+if __name__ == "__main__":
+    main()
