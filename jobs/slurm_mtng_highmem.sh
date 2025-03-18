e5a2bdd2ab06fb1cebf8f948cc93da59eb626c05
Matthew Ho <matthew.annam.ho@gmail.com>
2025-03-18 18:30:28 -0400

    running mtng/fastpm

--- jobs/slurm_mtng_highmem.sh
+++ jobs/slurm_mtng_highmem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=mtnglike_hm   # Job name
-#SBATCH --time=02:00:00         # Time limit
+#SBATCH --time=12:00:00         # Time limit
 #SBATCH --account=phy240043   # Account name
 #SBATCH --output=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out  # Output file for each array task
 #SBATCH --error=/anvil/scratch/x-mho1/jobout/%x_%A_%a.out   # Error file for each array task
@@ -14,8 +14,13 @@
 
 # SLURM_ARRAY_TASK_ID=400
 offset=0
-task_ids=(401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 418 419 420 421 422 423 424 425 426 427 428 429 430 682)
+task_ids=0
+# task_ids=(401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 418 419 420 421 422 423 424 425 426 427 428 429 430 682)
 # task_ids=(430 457 481 548 605 662 682 733 734 735 736 737 738 739 740 741 742 743 744 745 746 747 748 749 750 751 752 753 754 755 756 757 759 760 942 969)
+
+nbody=mtng
+sim=fastpm
+extras="meta.cosmofile=./params/mtng_cosmologies.txt"
 
 
 module restore cmass
@@ -23,7 +28,7 @@ conda activate cmassrun
 
 # Command to run for each lhid
 cd /home/x-mho1/git/ltu-cmass-run
-outdir=/anvil/scratch/x-mho1/cmass-ili/mtnglike/fastpm/L3000-N384
+outdir=/anvil/scratch/x-mho1/cmass-ili/$nbody/$sim/L3000-N384
 
 
 for SLURM_ARRAY_TASK_ID in "${task_ids[@]}"; do
@@ -37,8 +42,8 @@ for SLURM_ARRAY_TASK_ID in "${task_ids[@]}"; do
         echo "File $file exists."
     else
         echo "File $file does not exist."
-        python -m cmass.nbody.fastpm nbody=mtnglike nbody.lhid=$lhid +nbody.postprocess=False
+        python -m cmass.nbody.fastpm nbody=$nbody nbody.lhid=$lhid +nbody.postprocess=True $extras
 
-        sbatch --export=offset=$offset --array=$SLURM_ARRAY_TASK_ID jobs/slurm_mtng_postprocess.sh 
+        # sbatch --export=offset=$offset --array=$SLURM_ARRAY_TASK_ID jobs/slurm_mtng_postprocess.sh 
     fi
 done
