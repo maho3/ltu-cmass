0f192836a8b5ee23f284a201c432fff7e02e024e
Matthew Ho <matthew.annam.ho@gmail.com>
2025-05-14 11:13:14 -0400

    adding quijotelike bias slurm script

---
+++ jobs/slurm_quijotelike_bias.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#SBATCH --job-name=quijotelike_bias   # Job name
+#SBATCH --array=0-999         # Job array range for lhid
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks=5            # Number of tasks
+#SBATCH --gpus=v100:1     # Number of GPUs
+#SBATCH --time=04:00:00         # Time limit
+#SBATCH --partition=GPU-shared      # Partition name
+#SBATCH --account=phy240015p   # Account name
+#SBATCH --output=/ocean/projects/phy240015p/mho1/jobout/%x_%A_%a.out  # Output file for each array task
+#SBATCH --error=/ocean/projects/phy240015p/mho1/jobout/%x_%A_%a.out   # Error file for each array task
+
+# SLURM_ARRAY_TASK_ID=145
+globoffset=0
+
+module restore cmass
+conda activate cmass
+lhid=$SLURM_ARRAY_TASK_ID
+
+
+# Command to run for each lhid
+cd /jet/home/mho1/git/ltu-cmass
+
+Nhod=5
+Naug=1
+
+nbody=quijote
+sim=nbody_leauthaud
+multisnapshot=False
+diag_from_scratch=True
+rm_galaxies=False
+extras="bias.hod.model=leauthaud11 bias.hod.default_params=behroozi10" # meta.cosmofile=./params/big_sobol_params.txt" # "nbody.zf=0.500015"
+L=1000
+N=128
+
+extras="$extras hydra/job_logging=disabled"
+
+outdir=/ocean/projects/phy240015p/mho1/cmass-ili/$nbody/$sim/L$L-N$N
+echo "outdir=$outdir"
+
+
+for offset in 0 1000; do
+    lhid=$(($SLURM_ARRAY_TASK_ID+offset+globoffset))
+
+    postfix="nbody=$nbody sim=$sim nbody.lhid=$lhid multisnapshot=$multisnapshot diag.from_scratch=$diag_from_scratch $extras"
+
+    # density
+    # python -m cmass.diagnostics.summ diag.density=True $postfix 
+
+    # halos
+    file=$outdir/$lhid/halos.h5
+    if [ -f $file ]; then
+        echo "File $file exists."
+    else
+        echo "File $file does not exist."
+        # python -m cmass.bias.rho_to_halo $postfix
+    fi
+    # python -m cmass.diagnostics.summ diag.halo=True $postfix 
+
+    # galaxies
+    for i in $(seq 0 $(($Nhod-1))); do
+        hod_seed=$((lhid*10+i+1))
+        printf -v hod_str "%05d" $hod_seed
+        file=$outdir/$lhid/galaxies/hod$hod_str.h5
+        if [ -f $file ]; then
+            echo "File $file exists."
+        else
+            echo "File $file does not exist."
+            python -m cmass.bias.apply_hod $postfix bias.hod.seed=$hod_seed
+        fi
+        python -m cmass.diagnostics.summ $postfix diag.galaxy=True bias.hod.seed=$hod_seed
+
+        # # augments
+        # for aug_seed in $(seq 0 $(($Naug-1))); do
+        #     printf -v aug_str "%05d" $aug_seed
+        #     # lightcone
+        #     file=$outdir/$lhid/sgc_lightcone/hod${hod_str}_aug${aug_str}.h5
+        #     if [ -f $file ]; then
+        #         echo "File $file exists."
+        #     else
+        #         echo "File $file does not exist."
+        #         python -m cmass.survey.selection survey=cmass_sgc $postfix bias.hod.seed=$hod_seed survey.aug_seed=$aug_seed
+        #     fi
+        #     python -m cmass.diagnostics.summ diag.sgc=True bias.hod.seed=$hod_seed survey.aug_seed=$aug_seed $postfix 
+        # done
+
+        # Trash collection
+        if [ $rm_galaxies = True ]; then
+            # galaxies
+            echo "Removing galaxies for lhid=$lhid hod_seed=$hod_seed"
+            rm $outdir/$lhid/galaxies/hod$hod_str.h5
+
+            # lightcone
+            echo "Removing lightcone for lhid=$lhid hod_seed=$hod_seed"
+            rm $outdir/$lhid/sgc_lightcone/hod${hod_str}_aug*.h5
+        fi
+    done
+done
