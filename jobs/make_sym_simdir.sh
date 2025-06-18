27a5bf15dfe0c9f0551bd754e49e2341609d75fb
Matthew Ho <matthew.annam.ho@gmail.com>
2025-06-18 15:26:31 -0400

    updating make symlink script to adapt to different directories

--- jobs/make_sym_simdir.sh
+++ jobs/make_sym_simdir.sh
@@ -2,20 +2,18 @@
 
 set -e
 
-BASE_DIR="/ocean/projects/phy240015p/mho1/cmass-ili"
-SUITE="quijotelike"
-SIM1="fastpm"
-SIM2="fastpm_varnoise"
+SIM1="/ocean/projects/phy240015p/mho1/cmass-ili/quijotelike/fastpm"
+SIM2="/ocean/projects/phy240015p/mho1/cmass-ili/quijotelike/fastpm_varnoise"
 SUBDIR="L1000-N128"
 N=1999
 
-cd "$BASE_DIR/$SUITE/$SIM2/$SUBDIR"
+cd "$SIM2/$SUBDIR"
 
 for i in $(seq 0 "$N"); do
   (
     mkdir -p "$i" && cd "$i"
-    ln -s "../../../$SIM1/$SUBDIR/$i/config.yaml" .
-    ln -s "../../../$SIM1/$SUBDIR/$i/halos.h5" .
+    ln -s "$SIM1/$SUBDIR/$i/config.yaml" .
+    ln -s "$SIM2/$SUBDIR/$i/halos.h5" .
     echo "$i"
   )
 done
