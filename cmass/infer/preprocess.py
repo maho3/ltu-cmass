e1297e0de466c18a87edaa79342563ead70c326c
Nicolas Chartier <47000650+CompiledAtBirth@users.noreply.github.com>
2026-03-05 22:04:06 +0100

    Cross-validation on the test set for hyperparameter tuning (Optuna) (#146)
    * cross valuditation optuna objetive - to redo because I think the inner/outer loops are conceptually wrong
    
    * outer and inner loop of cross-validation traning with optuna implemented; TODO: routines to run metrics and use optuna studies ensembles if outer loop cv training was done
    
    * Cross validation optuna objective, only on test set
    
    * my custom infer conf file
    
    * some errors solved after mock training debugging
    
    * backup of my configuration files
    
    * returns ids for load_preprocess_data; next commit: pass them to objective_cval
    
    * using GroupShuffleSplit to condition the splits on the ids (cosmologies) during cross-validation for hyperparameters
    
    * error cleaning in optuna and adding retrain_optuna for after cross-validation
    
    * some errors corrected ; REMAINING: config in run_run_retraining_after_cval should be a struct and not a dict
    
    * changing dict to OmegaConf DictConfig in retraining script
    
    * correcting error when dumping config file after network retraining
    
    * correcting miskake: ema smoothing method was not passed to objective_cval nor retraining
    
    * changing load_ensemble in cmass.infer.validate so that we handle cross-validation cases
    
    * removing old infer configurations, and adding CV config to simple.yaml
    
    * returning global.yaml to the original configuration
    
    * Revert "returning global.yaml to the original configuration"
    
    This reverts commit 3518d30968f09f2cc0d978960fd2a7c666b61c13.
    
    * reverting global configuration, but keeping the summ_dir option for if users don't have write access
    
    * small autoformatting fixes
    
    * cleaning up code formatting in optuna.py
    
    * cleaning up more comments and code formatting in optuna.py
    
    * cleaning up more code formatting
    
    * having optuna only save timing and log_prob statistics
    
    * not saving individual split models anymore, just their performance metrics
    
    ---------
    
    Co-authored-by: Matthew Ho <matthew.annam.ho@gmail.com>

--- cmass/infer/preprocess.py
+++ cmass/infer/preprocess.py
@@ -257,8 +257,17 @@ def run_preprocessing(summaries, parameters, ids, hodprior, noiseprior,
 def main(cfg: DictConfig) -> None:
     cfg = parse_nbody_config(cfg)
 
+    logging.info("Scale factor a =  ", cfg.nbody.af)
+    # working dir where you have writing rights, ie to save preprocess splits
+    wdir = cfg.meta.wdir
+    # where the raw .h5 summaries are stored, only to read
+    summ_dir = cfg.meta.summ_dir
+
+    if summ_dir != wdir:
+        logging.info(f"Loading from separate summary directory: {summ_dir}")
+
     suite_path = get_source_path(
-        cfg.meta.wdir, cfg.nbody.suite, cfg.sim,
+        summ_dir, cfg.nbody.suite, cfg.sim,
         cfg.nbody.L, cfg.nbody.N, 0, check=False
     )[:-2]  # get to the suite directory
     model_dir = join(cfg.meta.wdir, cfg.nbody.suite, cfg.sim, 'models')
