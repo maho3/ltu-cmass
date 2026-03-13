f9260ca518b804d351900b3aaa142c901429fb54
Matt Ho <matthew.annam.ho@gmail.com>
2026-03-13 19:43:05 +0100

    Small feature additions to cmass.infer (#151)
    * Add funnel embedding and multihead funnel
    
    * add feature standardization before PCAA
    
    * compile the embedding architectures for SPEED
    
    * refactor Funnel to inherit from MLP
    
    * bugfixes

--- cmass/infer/architectures.py
+++ cmass/infer/architectures.py
@@ -103,3 +103,73 @@ class MultiHeadEmbedding(nn.Module):
             
         # Concatenate the embeddings
         return torch.cat(embeddings, dim=-1)
+
+
+class FunnelNetwork(MLP):
+    """
+    Fully-connected network that linearly interpolates hidden features
+    between input and output.
+    """
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        hidden_depth: int,
+        act_fn: str = "ReLU",
+    ):
+        hidden_layers = torch.linspace(
+            in_features, out_features, hidden_depth+2
+        ).to(torch.int)[1:-1].tolist()
+        super().__init__(
+            in_features=in_features,
+            out_features=out_features,
+            hidden_layers=hidden_layers,
+            act_fn=act_fn,
+        )
+
+
+class MultiHeadFunnel(nn.Module):
+    """
+    Multi-head embedding architecture with funnel networks.
+    
+    Takes a vector input, breaks it up into sub-vectors, embeds each
+    individually with FunnelNetworks, and then concatenates the embeddings
+    into a new context vector.
+    """
+    def __init__(
+        self,
+        start_idx: List[int],
+        in_features: List[int],
+        out_features: List[int],
+        hidden_depth: List[int],
+        act_fn: str = "ReLU",
+    ):
+        super().__init__()
+        
+        self.start_idx = start_idx
+        self.embedding_nets = nn.ModuleList()
+        for i in range(len(start_idx)-1):
+            self.embedding_nets.append(
+                FunnelNetwork(
+                    in_features=in_features[i],
+                    out_features=out_features[i],
+                    hidden_depth=hidden_depth[i],
+                    act_fn=act_fn,
+                )
+            )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Split the input tensor into sub-vectors
+        sub_vectors = []
+        for i in range(len(self.start_idx)-1):
+            start = self.start_idx[i]
+            end = self.start_idx[i+1]
+            sub_vectors.append(x[..., start:end])
+        
+        # Embed each sub-vector
+        embeddings = []
+        for i, sub_vector in enumerate(sub_vectors):
+            embeddings.append(self.embedding_nets[i](sub_vector))
+            
+        # Concatenate the embeddings
+        return torch.cat(embeddings, dim=-1)
