5d88ad01715c69df82e5a2a8ebd5db6d2010d9c6
Matthew AnNam Ho <matthew.annam.ho@gmail.com>
2026-07-28 15:01:09 -0500

    further safeguards for massive 3gpch runs on delta

--- quijote_wn/gen_quijote_ic.sh
+++ quijote_wn/gen_quijote_ic.sh
@@ -8,6 +8,9 @@ prefix=/u/maho3/git/ltu-cmass
 datadir=$prefix/data/quijote/wn/N${N}
 basedir=$prefix/quijote_wn
 
+module load fftw/3.3.10-gcc13.3.1 gsl/2.8-gcc13.3.1
+export LD_LIBRARY_PATH=$FFTW_HOME/lib:$GSL_HOME/lib:$LD_LIBRARY_PATH
+
 # Make datadir if it doesn't exist
 mkdir -p $datadir
 
