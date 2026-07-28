350cfc3bacc8c89bad51bd8fe7459e226114f5b7
Matthew AnNam Ho <matthew.annam.ho@gmail.com>
2026-07-28 15:00:54 -0500

    running particle sims for nicolas

--- jobs/slurm_fastpm.sh
+++ jobs/slurm_fastpm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=abacusmore  # Job name
-#SBATCH --array=0-19         # Job array range for lhid
+#SBATCH --job-name=nicolas  # Job name
+#SBATCH --array=0-49         # Job array range for lhid
 #SBATCH --nodes=1               # Number of nodes
-#SBATCH --ntasks=128            # Number of tasks
-#SBATCH --mem=240G            # Amount of memory
+#SBATCH --ntasks=32            # Number of tasks
+#SBATCH --mem=64G            # Amount of memory
 #SBATCH --time=3:00:00         # Time limit
 #SBATCH --partition=cpu  # Partition name
 #SBATCH --account=bdne-delta-cpu  # Account name
@@ -13,7 +13,7 @@
 
 # SLURM_ARRAY_TASK_ID=1
 echo "SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID"
-baseoffset=2000
+baseoffset=0
 
 module load cray-mpich/8.1.32 gsl
 export LD_LIBRARY_PATH=/sw/rh9.4/spack/v1.0.0/sw/linux-x86_64_v2/gsl-2.8-zty4u3k/lib:$LD_LIBRARY_PATH
@@ -26,21 +26,21 @@ lhid=$((SLURM_ARRAY_TASK_ID + baseoffset))
 # Command to run for each lhid
 cd /u/maho3/git/ltu-cmass
 
-nbody=abacuslike
+nbody=quijotelike_for_nicolas
 sim=fastpm
-multisnapshot=True
-extras="nbody.matchIC=0" #  meta.cosmofile=./params/abacus_cosmologies.txt"
-L=2000
-N=256
+multisnapshot=False
+extras="nbody.matchIC=2 nbody.postprocess=False" #  meta.cosmofile=./params/abacus_cosmologies.txt"
+L=1000
+N=128
 
 outdir=/work/hdd/bdne/maho3/cmass-ili/$nbody/$sim/L$L-N$N
 echo "outdir=$outdir"
 
-# export TQDM_DISABLE=0
-# extras="$extras hydra/job_logging=disabled"
+export TQDM_DISABLE=0
+extras="$extras hydra/job_logging=disabled"
 
 
-for offset in $(seq 0 20 200); do
+for offset in 0; do # $(seq 0 20 200); do
     loff=$((lhid + offset))
     
     postfix="nbody=$nbody sim=$sim nbody.lhid=$loff multisnapshot=$multisnapshot $extras"
